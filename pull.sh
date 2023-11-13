c78e984f3abbd20f6e01d6f51819e826b1691f65
Jeremy <51220084+jeremy-rifkin@users.noreply.github.com>
2023-11-13 11:24:44 -0500

    Create mirror of 6216e185863f41d6f19ab850caabfff7326020d7

---
+++ pull.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "Removing files"
+rm -rfv cmake src CMakeLists.txt COPYING
+echo "Fetching"
+git clone https://github.com/davea42/libdwarf-code.git
+cd libdwarf-code
+git checkout "6216e185863f41d6f19ab850caabfff7326020d7" # v0.8.0
+cd ..
+echo "Copying files"
+mkdir -p src/lib
+mv -v libdwarf-code/CMakeLists.txt .
+mv -v libdwarf-code/COPYING .
+mv -v libdwarf-code/cmake .
+mv -v libdwarf-code/src/lib/libdwarf src/lib
+echo "Deleting cloned repo"
+rm -rf libdwarf-code
+echo "Cleaning up src/lib/libdwarf"
+cd src/lib/libdwarf
+rm -rfv ChangeLog* CHANGES CODINGSTYLE NEWS
+cd ../../..
+echo "Patching"
+patch < patches/CMakeLists.patch
+echo "Done"
